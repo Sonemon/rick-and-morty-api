222339b0da6b8e557b4e8b595bb4b101e6964125
MRGL <Sonemongoo@gmail.com>
2025-04-10 14:25:35 +0300

    Added 2nd view - list of characters with filter

--- characters/views.py
+++ characters/views.py
@@ -1,5 +1,7 @@
 import random
-from rest_framework import status
+
+from django.db.models import QuerySet
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -17,3 +19,17 @@ def get_random_character_view(request):
     random_character = Character.objects.get(pk=random_pk)
     serializer = CharacterSerializer(random_character)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CharacterListView(generics.ListAPIView):
+    """
+    Returns a list of characters from the Rick and Morty API for given name.
+    """
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Character.objects.all()
+        name = self.request.query_params.get("name")
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
